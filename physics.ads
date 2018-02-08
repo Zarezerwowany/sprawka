c5c0ddcb8e1d2164752785e9e5b71cda55a50a6f
Zarezerwowany <damianpiszka@o2.pl>
2018-02-08 19:57:02 +0100

    v0.4

---
+++ physics.ads
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int physics(){
+
+    float p;
+
+    p = a/23 * delta.Time();
+    return p;
+}
--- source.ads
+++ source.ads
@@ -1,10 +1,15 @@
 #include <iostream>
+#include "addons.ads"
+#include "variables.ads"
+#include "functions.ads"
+#include "physics.ads"
 
 int main(){
     std::cout << "Program version 0.3\n\n";
     std::cout << "ABC CBA";
     std::cout << "A\tA";
     std::cout << "[abc]";
+    set_a(a);
 
     return 0;
 }
