e13296fc596cd4f732fa1d2c5024833c61c581ec
Zarezerwowany <damianpiszka@o2.pl>
2018-02-08 19:44:28 +0100

    v.0.2

--- source.ads
+++ source.ads
@@ -1,8 +1,9 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Program version 0.1\n\n";
+    std::cout << "Program version 0.2\n\n";
     std::cout << "ABC CBA";
+    std::cout << "A\tA";
 
     return 0;
 }
